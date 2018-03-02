f0385eab4469251070928017dde2756946786ad1
alex v <alex@encrypt-s.com>
2018-03-03 00:48:44 +0100

    copy slice in array

--- gox13hash.go
+++ gox13hash.go
@@ -10,6 +10,8 @@ import "C"
 // Hash the provided data, returning a slice of the [32]byte containing the resulting hash.
 func Sum(data []byte) [32]byte {
   var cresstr [32]C.char
+  var retbuf [32]byte
   C.xcoin_hash(C.CString(string(data)), C.int(len(data)), &cresstr[0])
-  return [32]byte(C.GoStringN(&cresstr[0], 32))
+  copy(retbuf[:], []byte(C.GoStringN(&cresstr[0], 32)[:32]))
+  return retbuf
 }
